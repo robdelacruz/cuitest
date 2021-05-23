ecda383c367ae40c01bd814a03bd0def3cea114f
rob <robdelacruz@gmail.com>
2021-05-23 17:04:52 +0800

    create tables

--- t.go
+++ t.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
+	_ "github.com/mattn/go-sqlite3"
 	tb "github.com/nsf/termbox-go"
 )
 
@@ -12,16 +15,65 @@ var _log *log.Logger
 var _termW, _termH int
 
 func main() {
+	err := run(os.Args[1:])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+}
+func run(args []string) error {
 	flog, err := os.Create("./log.txt")
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer flog.Close()
 	_log = log.New(flog, "", 0)
 
+	sw, parms := parseArgs(args)
+
+	// [-i new_file]  Create and initialize db file
+	if sw["i"] != "" {
+		dbfile := sw["i"]
+		if fileExists(dbfile) {
+			return fmt.Errorf("File '%s' already exists. Can't initialize it.\n", dbfile)
+		}
+		createTables(dbfile)
+		return nil
+	}
+
+	// Need to specify a db file as first parameter.
+	if len(parms) == 0 {
+		s := `Usage:
+
+   Specify database file:
+	t <db file>
+
+   To initialize new database file:
+	t -i <new db file>
+
+`
+		fmt.Printf(s)
+		return nil
+	}
+
+	// Exit if db file doesn't exist.
+	dbfile := parms[0]
+	if !fileExists(dbfile) {
+		return fmt.Errorf(`Database file '%s' doesn't exist. Create one using:
+	t -i <filename>
+   `, dbfile)
+	}
+
+	//db, err := sql.Open("sqlite3", dbfile)
+	_, err = sql.Open("sqlite3", dbfile)
+	if err != nil {
+		return fmt.Errorf("Error opening '%s' (%s)\n", dbfile, err)
+	}
+
+	// Start termbox mode
 	err = tb.Init()
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer tb.Close()
 	tb.SetOutputMode(tb.Output256)
@@ -43,6 +95,49 @@ func main() {
 			mainwin.Draw()
 		}
 	}
+	return nil
+}
+
+func listContains(ss []string, v string) bool {
+	for _, s := range ss {
+		if v == s {
+			return true
+		}
+	}
+	return false
+}
+func fileExists(file string) bool {
+	_, err := os.Stat(file)
+	if err != nil && os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+//*** DB functions ***
+func sqlstmt(db *sql.DB, s string) *sql.Stmt {
+	stmt, err := db.Prepare(s)
+	if err != nil {
+		log.Fatalf("db.Prepare() sql: '%s'\nerror: '%s'", s, err)
+	}
+	return stmt
+}
+func sqlexec(db *sql.DB, s string, pp ...interface{}) (sql.Result, error) {
+	stmt := sqlstmt(db, s)
+	defer stmt.Close()
+	return stmt.Exec(pp...)
+}
+func txstmt(tx *sql.Tx, s string) *sql.Stmt {
+	stmt, err := tx.Prepare(s)
+	if err != nil {
+		log.Fatalf("tx.Prepare() sql: '%s'\nerror: '%s'", s, err)
+	}
+	return stmt
+}
+func txexec(tx *sql.Tx, s string, pp ...interface{}) (sql.Result, error) {
+	stmt := txstmt(tx, s)
+	defer stmt.Close()
+	return stmt.Exec(pp...)
 }
 
 func pollEvents(chev chan tb.Event) {
@@ -53,6 +148,90 @@ func pollEvents(chev chan tb.Event) {
 		}
 		chev <- e
 	}
+}
+
+func createTables(newfile string) {
+	if fileExists(newfile) {
+		s := fmt.Sprintf("File '%s' already exists. Can't initialize it.\n", newfile)
+		fmt.Printf(s)
+		os.Exit(1)
+	}
+
+	db, err := sql.Open("sqlite3", newfile)
+	if err != nil {
+		fmt.Printf("Error opening '%s' (%s)\n", newfile, err)
+		os.Exit(1)
+	}
+
+	ss := []string{
+		"CREATE TABLE accounttype (accounttype_id INTEGER PRIMARY KEY NOT NULL, name TEXT);",
+		"CREATE TABLE currency (currency_id INTEGER PRIMARY KEY NOT NULL, name TEXT, usdrate REAL);",
+		"CREATE TABLE account (account_id INTEGER PRIMARY KEY NOT NULL, code TEXT, name TEXT, accounttype_id INTEGER, currency_id INTEGER);",
+		"CREATE TABLE trans (trans_id INTEGER PRIMARY KEY NOT NULL, account_id INTEGER, date TEXT, ref TEXT, desc TEXT, amt REAL);",
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		log.Printf("DB error (%s)\n", err)
+		os.Exit(1)
+	}
+	for _, s := range ss {
+		_, err := txexec(tx, s)
+		if err != nil {
+			tx.Rollback()
+			log.Printf("DB error (%s)\n", err)
+			os.Exit(1)
+		}
+	}
+	err = tx.Commit()
+	if err != nil {
+		log.Printf("DB error (%s)\n", err)
+		os.Exit(1)
+	}
+}
+
+func parseArgs(args []string) (map[string]string, []string) {
+	switches := map[string]string{}
+	parms := []string{}
+
+	standaloneSwitches := []string{}
+	definitionSwitches := []string{"i"}
+	fNoMoreSwitches := false
+	curKey := ""
+
+	for _, arg := range args {
+		if fNoMoreSwitches {
+			// any arg after "--" is a standalone parameter
+			parms = append(parms, arg)
+		} else if arg == "--" {
+			// "--" means no more switches to come
+			fNoMoreSwitches = true
+		} else if strings.HasPrefix(arg, "--") {
+			switches[arg[2:]] = "y"
+			curKey = ""
+		} else if strings.HasPrefix(arg, "-") {
+			if listContains(definitionSwitches, arg[1:]) {
+				// -a "val"
+				curKey = arg[1:]
+				continue
+			}
+			for _, ch := range arg[1:] {
+				// -a, -b, -ab
+				sch := string(ch)
+				if listContains(standaloneSwitches, sch) {
+					switches[sch] = "y"
+				}
+			}
+		} else if curKey != "" {
+			switches[curKey] = arg
+			curKey = ""
+		} else {
+			// standalone parameter
+			parms = append(parms, arg)
+		}
+	}
+
+	return switches, parms
 }
 
 type MainWindow struct {
